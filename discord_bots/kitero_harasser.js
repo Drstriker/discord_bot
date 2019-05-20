80b56a16c247f74b2d7e0785e6ec6efaec273b52
Striker <drstriker666@gmail.com>
2019-05-20 16:56:02 +0200

    poke command Online

--- discord_bots/kitero_harasser.js
+++ discord_bots/kitero_harasser.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require("./../config.json");
+const config = require("./../kitero_harasser.config.json");
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -11,15 +11,10 @@ client.on('ready', () => {
 client.on('message', msg => {
     const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    // if (msg.content === "ping kitero") {
-    //     msg.channel.send(`${msg.author} POKED <@173536790861316096> !!`);
-    // }
-    // if (msg.content === "ping drstriker") {
-    //     msg.channel.send(`${msg.author} POKED <@183378946950168577> !!`);
-    // }
-    user = msg.mentions.users.second();
+
+    user = msg.mentions.users.first();
     if (msg.content === `poke ` + user) {
-        msg.channel.send(`${msg.author} POKED ${msg.member}!!`);
+        msg.channel.send(`${msg.author} POKED ` + user + ` !!`);
     }
     if (msg.content === "!authortest") {
         msg.channel.send(`Hello ${msg.author}`);
@@ -32,13 +27,9 @@ client.on('message', msg => {
     //     msg.delete();
     //     msg.channel.send(messtosay);
     // }
-    if(command === "!say") {
-        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-        // To get the "message" itself we join the `args` back into a string with spaces: 
+    if (command === "!say") {
         const sayMessage = args.join(" ");
-        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
         msg.delete().catch(O_o=>{}); 
-        // And we get the bot to say the thing: 
         msg.channel.send(sayMessage);
       }
     console.log(msg);
