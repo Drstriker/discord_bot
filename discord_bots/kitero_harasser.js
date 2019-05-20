33516b5f961561371557455c4d7cc735d881e350
Striker <drstriker666@gmail.com>
2019-05-20 16:50:50 +0200

    Init first commands & code structure reviewed

--- discord_bots/kitero_harasser.js
+++ discord_bots/kitero_harasser.js
@@ -1,23 +1,47 @@
 const Discord = require('discord.js');
-const token = 'NTc5OTkwNTQ5NDYxNzI5Mjgw.XOKO0A.digqlXh6tidP96ZR1_GHEs-AMHk';
-const client_id = '579990549461729280';
-
+const config = require("./../config.json");
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('Bot is now connected');
     const channel_name = "test";
-    client.channels.find(x => x.name === channel_name).send("Hello I\'m now connected to server " + channel_name);
+    //client.channels.find(x => x.name === channel_name).send("Hello I\'m now connected to server " + channel_name);
 });
 
 client.on('message', msg => {
-    if (msg.content === "ping kitero") {
-        msg.reply("YOU POKED KITERO !!");
+    const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    // if (msg.content === "ping kitero") {
+    //     msg.channel.send(`${msg.author} POKED <@173536790861316096> !!`);
+    // }
+    // if (msg.content === "ping drstriker") {
+    //     msg.channel.send(`${msg.author} POKED <@183378946950168577> !!`);
+    // }
+    user = msg.mentions.users.second();
+    if (msg.content === `poke ` + user) {
+        msg.channel.send(`${msg.author} POKED ${msg.member}!!`);
     }
-    if (msg.content === "!test") {
+    if (msg.content === "!authortest") {
         msg.channel.send(`Hello ${msg.author}`);
     }
+    if (msg.content === "!idtest") {
+        msg.channel.send(`Hello <@183378946950168577>`);
+    }
+    // if (msg.content === "!say") {
+    //     const messtosay = msg.content.join(" ");
+    //     msg.delete();
+    //     msg.channel.send(messtosay);
+    // }
+    if(command === "!say") {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+        // To get the "message" itself we join the `args` back into a string with spaces: 
+        const sayMessage = args.join(" ");
+        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+        msg.delete().catch(O_o=>{}); 
+        // And we get the bot to say the thing: 
+        msg.channel.send(sayMessage);
+      }
     console.log(msg);
 });
 
-client.login(token);
+client.login(config.token);
