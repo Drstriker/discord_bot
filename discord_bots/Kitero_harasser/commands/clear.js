9889dae1b35c34568cb38f01f82f84884c3cd8d6
Striker <drstriker666@gmail.com>
2019-05-27 12:06:53 +0200

    Clear bug fix

--- discord_bots/Kitero_harasser/commands/clear.js
+++ discord_bots/Kitero_harasser/commands/clear.js
@@ -2,15 +2,16 @@ module.exports.run = async(bot, msg, args) => {
     let default_number = 99;
     let msgArray = msg.content.split(" ");
 
-    if (msgArray) {
+    if (msgArray[1]) {
         msg.channel.bulkDelete(msgArray[1]).then(() => {
             msg.channel.send(`Cleared ${msgArray[1]} messages`);
         });
     }
-    else if (!msgArray)
-        msg.channel.bulkDelete(msgArray[1]).then(() => {
+    else {
+        msg.channel.bulkDelete(99).then(() => {
             msg.channel.send(`Cleared all messages`);
         });
+    }
 }
 
 module.exports.config = {
