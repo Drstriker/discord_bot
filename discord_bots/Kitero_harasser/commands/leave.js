c12cb6bd6cf3333279ab5d1827599ab7d7bb7559
Striker <drstriker666@gmail.com>
2019-05-27 12:07:27 +0200

    Leave fix

--- discord_bots/Kitero_harasser/commands/leave.js
+++ discord_bots/Kitero_harasser/commands/leave.js
@@ -1,7 +1,5 @@
 module.exports.run = async(bot, msg, args) => {
-    if (cmd == `${prefix}leave`) {
-        msg.member.voiceChannel.leave();
-    }
+    msg.member.voiceChannel.leave();
 }
 
 module.exports.config = {
