70012cb5cbf88888628a40f925d5e42406325bad
Thierry <thierry.yu@epitech.eu>
2019-09-10 23:49:14 +0200

    token change

--- discord_bots/Kitero_harasser/commands/poke.js
+++ discord_bots/Kitero_harasser/commands/poke.js
@@ -12,7 +12,6 @@ module.exports.run = async(bot, msg, args) => {
         }
         else
             msg.channel.send(`${msg.author} POKED ` + msg.mentions.users.first() + ` !!`);
-        console.log('Poke command from Command Handler');
     }
 }
 
